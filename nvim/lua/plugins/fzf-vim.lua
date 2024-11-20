483813e6d8d801cc8e65afb79c347de05b0bffb5
naoki0xff <cover551.9@gmail.com>
2024-11-20 13:02:47 +0900

    update

--- nvim/lua/plugins/fzf-vim.lua
+++ nvim/lua/plugins/fzf-vim.lua
@@ -6,6 +6,10 @@ return {
   {
     'ibhagwan/fzf-lua',
     dependencies = { 'junegunn/fzf', 'nvim-tree/nvim-web-devicons' },
+    -- binary dependencies:
+    -- https://github.com/sharkdp/fd
+    -- https://github.com/BurntSushi/ripgrep
+    -- https://github.com/sharkdp/bat
     config = function()
       require("fzf-lua").setup({
         winopts = {
