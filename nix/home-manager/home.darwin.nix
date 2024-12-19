4e1b5d9c8b762e254ade845acf229278dce1478e
naoki0xff <cover551.9@gmail.com>
2024-12-19 11:49:17 +0900

    update

--- nix/home-manager/home.darwin.nix
+++ nix/home-manager/home.darwin.nix
@@ -1,38 +1,32 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
+  # HOME Config
   home.username = "naoki";
   home.homeDirectory = "/Users/naoki";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = [
-    pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+  # Install Nix Packages
+  # Check available nixpkgs => https://search.nixos.org/packages
+  # List of available modules => https://github.com/nix-community/home-manager/tree/master/modules/programs
+  imports = [
+    ./pkgs/editor.nix
+    ./pkgs/languages.nix
+    #./pkgs/shell.nix
+    ./pkgs/utils.nix
   ];
+
+  # # It is sometimes useful to fine-tune packages, for example, by applying
+  # # overrides. You can do that directly here, just don't forget the
+  # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+  # # fonts?
+  # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+  # # You can also create simple shell scripts directly inside your
+  # # configuration. For example, this adds a command 'my-hello' to your
+  # # environment:
+  # (pkgs.writeShellScriptBin "my-hello" ''
+  #   echo "Hello, ${config.home.username}!"
+  # '')
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
@@ -68,6 +62,17 @@
   home.sessionVariables = {
     # EDITOR = "emacs";
   };
+
+  # See home manager options => https://nix-community.github.io/home-manager/options.xhtml#opt-home.file
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.11"; # Please read the comment before changing.
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
--- nix/home-manager/pkgs/shell.nix
+++ nix/home-manager/pkgs/shell.nix
@@ -1,6 +1,72 @@
 { config, pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-  ];
+  program.zsh = {
+    enable = true;
+
+    sessionVariables = {
+    };
+
+    shellAliases = {
+      # General
+      ls      = "ls -CF";
+      sl      = "ls -CF";
+      ll      = "ls -1";
+      lt      = "ls -1t";
+      la      = "ls -CFalh";
+      lls     = "tree -N -L 2";
+      lla     = "tree -a -h -N -L 2";
+      mv      = "mv -i";
+      cp      = "cp -i";
+      whihc   = "which";
+      wl      = "wc -l";
+      mkdir   = "mkdir -p";
+      grep    = "grep --color=auto";
+      sed     = "gsed";
+      diff    = "colordiff -u";
+      less    = "less -NR";
+      fzf     = "fzf-tmux";
+      hm      = "home-manager";
+
+      # Editor
+      vi      = "nvim -u NONE";
+      vim     = "nvim";
+      vimdiff = "nvim -d";
+      taginit = "ctags -R -f .tags";
+
+      # Git
+      gs      = "git status";
+      ga      = "git add";
+      gc      = "git commit";
+      gcm     = "git commit -m";
+      gp      = "git push";
+      gd      = "git diff";
+      gb      = "git branch";
+      gba     = "git branch -a";
+#TODO: make those too long oneliner to escaped multi line
+      gbs     = "(){ TARGETBRANCH=`git branch -a | sed 's/ *//g' | fzf-tmux -p`; if [ -n ${TARGETBRANCH} ]; then if [[ ${TARGETBRANCH} == 'remotes/origin/'* ]]; then TARGETBRANCH=${TARGETBRANCH/remotes\/origin\//}; fi; git checkout $TARGETBRANCH; unset TARGETBRANCH; fi }";
+      gl      = "git log --stat";
+      glo     = "git log --oneline";
+      glp     = "git log -p";
+      glm     = "git log -m --name-status";
+      gss     = "(){ if [ -n '${1}' ];then git stash push -u -m $1; else git stash push -u; fi }";
+      gsx     = "(){ TARGETSTASH=`git stash list | cut -d':' -f1 | fzf-tmux -p`; if [ -z ${TARGETSTASH} ]; then echo 'DROP not done'; else git stash drop ${TARGETSTASH}; unset TARGETSTASH ;fi }";
+      gsl     = "git stash list";
+      gsd     = "git diff `git stash list | cut -d':' -f1 | fzf-tmux -p`";
+      gsa     = "(){ TARGETSTASH=`git stash list | cut -d':' -f1 | fzf-tmux -p`; if [ -z ${TARGETSTASH} ]; then echo 'APPLY not done'; else git stash apply ${TARGETSTASH}; unset TARGETSTASH ;fi }";
+
+      # Development
+      o       = "open .";
+      d       = "podman";
+      dc      = "podman compose";
+      docker  = "podman";
+      p       = "podman";
+      pd      = "podman compose";
+      tf      = "terraform";
+      tg      = "terragrunt";
+      k       = "kubectl";
+      kcx     = "kubectx";
+      kcn     = "kubens";
+    };
+  };
 }
