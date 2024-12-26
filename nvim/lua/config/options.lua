99561166bdb4f634f9efd1720b64c2f8c07284c2
naoki0xff <cover551.9@gmail.com>
2024-12-26 19:58:01 +0900

    update

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,64 +1,62 @@
-local opt = vim.opt
-
 -- Encoding
-opt.encoding = 'utf-8'
+vim.opt.encoding = 'utf-8'
 vim.scriptencoding = 'utf-8'
 
 -- Apperence
-opt.termguicolors = true
-opt.winblend = 15
-opt.number = true
-opt.display = 'lastline'
-opt.pumheight = 10
-opt.laststatus = 3
-opt.ambiwidth = 'double'
-opt.completeopt:remove({ 'preview' })
-opt.lazyredraw = true
-opt.shortmess:append({ c = true })
-opt.signcolumn = 'yes'
+vim.opt.termguicolors = true
+vim.opt.winblend = 15
+vim.opt.number = true
+vim.opt.display = 'lastline'
+vim.opt.pumheight = 10
+vim.opt.laststatus = 3
+vim.opt.ambiwidth = 'double'
+vim.opt.completeopt:remove({ 'preview' })
+vim.opt.lazyredraw = true
+vim.opt.shortmess:append({ c = true })
+vim.opt.signcolumn = 'yes'
 
 -- Buffer
-opt.hidden = true
+vim.opt.hidden = true
 
 -- Backup
-opt.autoread = true
-opt.backup = true
-opt.backupdir = os.getenv("HOME") .. '/.local/share/nvim/backup'
-opt.undofile = true
-opt.undodir = os.getenv("HOME") .. '/.local/share/nvim/undo'
+vim.opt.autoread = true
+vim.opt.backup = true
+vim.opt.backupdir = os.getenv("HOME") .. '/.local/share/nvim/backup'
+vim.opt.undofile = true
+vim.opt.undodir = os.getenv("HOME") .. '/.local/share/nvim/undo'
 
 -- Session
-opt.sessionoptions:append({ 'winpos', 'terminal', 'folds' })
+vim.opt.sessionoptions:append({ 'winpos', 'terminal', 'folds' })
 
 -- Clipboard
-opt.clipboard:append({ 'unnamedplus' })
+vim.opt.clipboard:append({ 'unnamedplus' })
 
 -- Mouse
-opt.mouse = 'a'
+vim.opt.mouse = 'a'
 
 -- Edit
-opt.tabstop = 2
-opt.shiftwidth = 2
-opt.expandtab = true
-opt.autoindent = true
-opt.smartindent = true
-opt.breakindent = true
-opt.completeopt = 'menu,menuone,noselect,preview'
-opt.wildoptions = 'pum'
-opt.backspace = 'indent,eol,start'
-opt.virtualedit = 'block'
-opt.diffopt = 'internal,filler,indent-heuristic,algorithm:histogram'
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.breakindent = true
+vim.opt.completeopt = 'menu,menuone,noselect,preview'
+vim.opt.wildoptions = 'pum'
+vim.opt.backspace = 'indent,eol,start'
+vim.opt.virtualedit = 'block'
+vim.opt.diffopt = 'internal,filler,indent-heuristic,algorithm:histogram'
 
 -- Search
-opt.hlsearch = true
-opt.incsearch = true
-opt.wrapscan = true
-opt.ignorecase = true
-opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.wrapscan = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 -- ctags
-opt.tags = '.tags;~'
+vim.opt.tags = '.tags;~'
 
 -- MISC
-opt.keywordprg = ":help"
-opt.fixeol = false
+vim.opt.keywordprg = ":help"
+vim.opt.fixeol = false
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -31,7 +31,32 @@ return {
             capabilities = capabilities,
           }
           -- Extra Configuration for each language server
-          -- YAML
+          -- LUA: Enable LSP support for vim config files
+          require'lspconfig'.lua_ls.setup {
+            on_init = function(client)
+              if client.workspace_folders then
+                local path = client.workspace_folders[1].name
+                if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+                  return
+                end
+              end
+              client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+                runtime = {
+                  version = 'LuaJIT'
+                },
+                workspace = {
+                  checkThirdParty = false,
+                  library = {
+                    vim.env.VIMRUNTIME
+                  }
+                }
+              })
+            end,
+            settings = {
+              Lua = {}
+            }
+          }
+          -- YAML: Enable completion for various sources
           require('lspconfig').yamlls.setup {
             settings = {
               yaml = {
