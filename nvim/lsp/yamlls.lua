e9426e3489a3cc11250333f92584562f3df24d1e
naoki0xff <cover551.9@gmail.com>
2025-07-05 01:14:20 +0900

    update

--- nvim/lsp/yamlls.lua
+++ nvim/lsp/yamlls.lua
@@ -1,4 +1,4 @@
 return {
   cmd = { "yaml-language-server", "--stdio" },
-  filetypes = { "yaml", "yaml.docker-compose", "yaml.gitlab", "yaml.helm-values" },
+  filetypes = { "yaml", "yaml.docker-compose", "yaml.gitlab", },
 }
