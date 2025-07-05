814d7b85b5b49e75976023e64edfcba893a033ee
naoki0xff <cover551.9@gmail.com>
2025-07-05 15:58:57 +0900

    update

--- nvim/lsp/jdtls.lua
+++ nvim/lsp/jdtls.lua
@@ -1,5 +1,11 @@
+local user_cache_dir = os.getenv("XDG_CACHE_HOME") or os.getenv("HOME") .. "/.cache"
+
 return {
-  cmd = { "jdtls", "-configuration", "/home/runner/.cache/jdtls/config", "-data", "/home/runner/.cache/jdtls/workspace" },
+  cmd = { "jdtls", "-configuration", user_cache_dir .. "/jdtls/config", "-data", user_cache_dir .. "/jdtls/workspace" },
   filetypes = { "java" },
+  init_options = {
+    jvm_args = {},
+    workspace = user_cache_dir .. "/jdtls/workspace"
+  },
   root_markers = { ".git", "build.gradle", "build.gradle.kts", "build.xml", "pom.xml", "settings.gradle", "settings.gradle.kts" },
 }
--- nvim/lsp/phpactor.lua
+++ nvim/lsp/phpactor.lua
@@ -1,5 +1,6 @@
 return {
   cmd = { "phpactor", "language-server" },
   filetypes = { "php" },
+  workspace_required = true,
   root_markers = { ".git", "composer.json", ".phpactor.json", ".phpactor.yml" },
 }
